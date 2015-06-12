f8379a846fd99f436f4246d4b49d896d6b56f797
miosec <miosec@gmail.com>
2015-06-13 02:12:04 +0800

    初始导入

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # mobilesafe
+2015-06-13 2:11
+	/**
+		 * 想要实现的功能 :
+		 * 
+		 * 去服务器获取版本信息,进行比对,如果相同,则跳转到主的Activity,否则则去更新为服务器的版本.
+		 * 
+		 * 分析:
+		 * 
+		 * 1.网络获取服务器版本进行比对
+		 * 2.如果不同则弹出版本更新对话框,标题为最新版本:+ServerVersionCode,内容为json返回的desc信息,
+		 * 选择升级则进入下载更新线程,否则跳转到主的activity
+		 * 3.下载更新使用xutils,并且使用一个textview控件连显示实时更新的进度
+		 * .更新完毕,跳转到系统的安装apk的意图进行替换安装,如果失败则跳转到主activity
+		 * 
+		 */
+该功能使用自己的封装的LaoUtils类一句话实现
+	//通过封装一句话搞定获取版本和显示版本及下载更新方法.
+		LaoUtils.getVersionAndShowUpdateDialog(
+				getResources().getString(R.string.update_url), "GET", 5000,
+				this, "code", "desc", "update_url", "新版本",
+				R.drawable.ic_launcher, "org.miosec.mobilesafe.HomeActivity");
--- src/org/miosec/mobilesafe/activity/SplashActivity.java
+++ src/org/miosec/mobilesafe/activity/SplashActivity.java
@@ -16,28 +16,11 @@ public class SplashActivity extends BasicActivity {
  
 	@Override
 	public void init() {
-		System.out.println("init");
 		TextView splash_version = (TextView) findViewById(R.id.splash_version);
 	}
 
 	@Override
 	public void controller() {
-		System.out.println("controller");
-		/**
-		 * 想要实现的功能 :
-		 * 
-		 * 去服务器获取版本信息,进行比对,如果相同,则跳转到主的Activity,否则则去更新为服务器的版本.
-		 * 
-		 * 分析:
-		 * 
-		 * 1.网络获取服务器版本进行比对
-		 * 2.如果不同则弹出版本更新对话框,标题为最新版本:+ServerVersionCode,内容为json返回的desc信息,
-		 * 选择升级则进入下载更新线程,否则跳转到主的activity
-		 * 3.下载更新使用xutils,并且使用一个textview控件连显示实时更新的进度
-		 * .更新完毕,跳转到系统的安装apk的意图进行替换安装,如果失败则跳转到主activity
-		 * 
-		 */
-
 		//通过封装一句话搞定获取版本和显示版本及下载更新方法.
 		LaoUtils.getVersionAndShowUpdateDialog(
 				getResources().getString(R.string.update_url), "GET", 5000,
